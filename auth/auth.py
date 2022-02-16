751200676af8016d1d141273f8d009b8a0ce7a3c
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2022-02-15 21:59:42 -0300

    Triggering function by an HTTP request

--- auth/auth.py
+++ auth/auth.py
@@ -1,5 +1,19 @@
+import os
 import jwt
 import json
 
+from dotenv import load_dotenv
+
+load_dotenv()
+AUTH_SECRET = os.getenv('AUTH_SECRET')
+
 def authenticate(event, context):
-    pass
+    encoded_jwt = jwt.encode({"some": "payload"}, AUTH_SECRET)
+
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+        "token": str(encoded_jwt)
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
